51701d5df9f67d927db7086df0e48de99365f52f
Tesa44 <mateusz.tesarewicz123@gmail.com>
2025-05-20 10:34:55 +0200

    create list of friends

--- src/App.js
+++ src/App.js
@@ -7,14 +7,57 @@ const initialFriends = [
   },
   {
     id: 933372,
-    name: "Sarah",
+    name: "Mia",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
+    name: "Nathaniel",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
 ];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList></FriendsList>
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id}></Friend>
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name}></img>
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and your friend are even</p>}
+      <button className="button">Select</button>
+    </li>
+  );
+}
